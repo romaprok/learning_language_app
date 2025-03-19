49fe6fd69ac932e77199f449926c0bca8eaf21b1
Roman Prokopets <53170216+romaprok@users.noreply.github.com>
2025-03-19 18:34:59 +0200

    chore: add pre commit hooks (#2)
    * test(feature-test): test
    
    * chore(feature-pre-commit): setup pre-commit hooks
    
    * chore(feature-pre-commit): remove log

---
+++ commitlint.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'type-case': [2, 'always', 'lowercase'],
+    'type-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'lowercase'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'header-max-length': [2, 'always', 72]
+  }
+}; 
