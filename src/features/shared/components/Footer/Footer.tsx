d2735c59aeabc841f7ec32cfc15d09e0362b9033
romaprok <roman93prokopets@gmail.com>
2025-03-20 11:19:04 +0200

    chore(add-css-modules): removed unused files

--- src/features/shared/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles/Footer.module.scss';
-
-export function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <p>© 2024 Language Learning App - Your Gateway to New Languages</p>
-    </footer>
-  );
-} 
--- src/features/shared/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles/Header.module.scss';
-
-export function Header() {
-  return (
-    <header className={styles.header}>
-      <h1>Welcome to Language Learning App!</h1>
-    </header>
-  );
-} 
